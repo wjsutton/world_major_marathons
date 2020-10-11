c09f13f51490318c45a76260d9ae122e832e0597
Will Sutton <sutton_999@hotmail.com>
2020-10-11 16:41:55 +0100

    updating data process

---
+++ clean_wmm_data.R
@@ -0,0 +1,87 @@
+
+d1 <- readRDS("data/raw_data_output.RDS")
+
+for(i in seq_along(d1)){
+  print(i)
+  
+  runner_id <- d1[[i]]$id
+  name <- d1[[i]]$name
+  profile_url <- d1[[i]]$url
+  nationality <- d1[[i]]$nationality_iaaf
+  
+  toyko_time <- ifelse(length(d1[[i]]$stars[[1]])==2,
+                       gsub('&nbsp;','',d1[[i]]$stars[[1]]$finish_time),
+                       NA)
+  
+  chicago_time <- ifelse(length(d1[[i]]$stars[[2]])==2,
+                         gsub('&nbsp;','',d1[[i]]$stars[[2]]$finish_time),
+                         NA)
+  
+  berlin_time <- ifelse(length(d1[[i]]$stars[[3]])==2,
+                        gsub('&nbsp;','',d1[[i]]$stars[[3]]$finish_time),
+                        NA)
+  
+  newyorkcity_time <- ifelse(length(d1[[i]]$stars[[4]])==2,
+                             gsub('&nbsp;','',d1[[i]]$stars[[4]]$finish_time),
+                             NA)
+  
+  boston_time <- ifelse(length(d1[[i]]$stars[[5]])==2,
+                        gsub('&nbsp;','',d1[[i]]$stars[[5]]$finish_time),
+                        NA)
+  
+  london_time <- ifelse(length(d1[[i]]$stars[[6]])==2,
+                        gsub('&nbsp;','',d1[[i]]$stars[[6]]$finish_time),
+                        NA)
+  
+  
+  toyko_year <- ifelse(length(d1[[i]]$stars[[1]])==2,
+                       d1[[i]]$stars[[1]]$year,
+                       NA)
+  
+  chicago_year <- ifelse(length(d1[[i]]$stars[[2]])==2,
+                         d1[[i]]$stars[[2]]$year,
+                         NA)
+  berlin_year <- ifelse(length(d1[[i]]$stars[[3]])==2,
+                        d1[[i]]$stars[[3]]$year,
+                        NA)
+  newyorkcity_year <- ifelse(length(d1[[i]]$stars[[4]])==2,
+                             d1[[i]]$stars[[4]]$year,
+                             NA)
+  
+  boston_year <- ifelse(length(d1[[i]]$stars[[5]])==2,
+                        d1[[i]]$stars[[5]]$year,
+                        NA)
+  
+  london_year <- ifelse(length(d1[[i]]$stars[[6]])==2,
+                        d1[[i]]$stars[[6]]$year,
+                        NA)
+  
+  df <- data.frame(
+    runner_id,
+    name,
+    profile_url,
+    nationality,
+    toyko_time,
+    chicago_time,
+    berlin_time,
+    newyorkcity_time,
+    boston_time,
+    london_time,
+    toyko_year,
+    chicago_year,
+    berlin_year,
+    newyorkcity_year,
+    boston_year,
+    london_year,
+    stringsAsFactors = F
+  )
+  
+  if(i == 1){
+    marathon_results <- df
+  }
+  if(i != 1){
+    marathon_results <- rbind(marathon_results,df)
+  }
+}
+marathon_results <- unique(marathon_results)
+write.csv(marathon_results,"data/marathon_results.csv", row.names = F)
---
+++ run_wmm_calls_and_save_data.R
@@ -0,0 +1,21 @@
+
+library(jsonlite)
+
+calls <- read.csv('data/wmm_call_list.csv',stringsAsFactors = F)
+
+for(a in seq_along(calls$x)){
+  hof_ajax <- calls$x[a]
+  Sys.sleep(30)
+  data <- read_json(hof_ajax)
+  d1 <- data$objects
+  
+  if(a == 1){
+    output <- d1
+  }
+  if(a != 1){
+    output <- c(output,d1)
+  }
+  print(paste0(a," done!"))
+}
+
+saveRDS(output,"data/raw_data_output.RDS")
